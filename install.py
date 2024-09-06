e3a6c2ab363cd880faabadae6fb59c600943c859
Somraj Saha <somraj.saha@jarmos.dev>
2024-09-06 18:07:22 +0530

    chore: add basic code to automate the self-hosted runners

--- install.py
+++ install.py
@@ -14,10 +14,32 @@ Follow the instructions below for a brief usage guideline of the script:
        number of runners to deploy on the host.
 """
 
+import argparse
+
 
 def main() -> None:
     """Entrypoint of the script."""
-    print("Hello World!")
+    # Initialise the ArgumentParser object
+    parser = argparse.ArgumentParser(
+        description="install and setup the self-hosted GitHub Action runners."
+    )
+
+    # Add the arguments for the script
+    parser.add_argument(
+        "-n",
+        "--number",
+        default=1,
+        help="the number of runners to spin up (default: 1)",
+    )
+
+    # Generate the list of arguments for further processing
+    args = parser.parse_args()
+
+    # TODO(Somraj Saha): Handle installation and setup of the runners according to the
+    # number of runners specified.
+    # WEBUR-18
+    for n in range(int(args.number)):
+        print(f"{n + 1}: 'Hello World!'")
 
 
 if __name__ == "__main__":
