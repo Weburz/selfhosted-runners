af93ba2cb486ca2e2722c9520d1793a9cf4e6820
Somraj Saha <somraj.saha@jarmos.dev>
2024-09-10 14:33:02 +0530

    chore: add a script to remove the runners

---
+++ remove.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python3
+
+"""Script to uninstall and remove the self-hosted GitHub Action runners.
+
+The script is made publicly available under the terms and conditions of the MIT
+license. You will find more information about the distribution and usage copyright
+guidelines in the LICENSE file linked below:
+https://github.com/Weburz/selfhosted-runners/blob/main/LICENSE
+
+Follow the instructions below for a brief usage guideline of the script:
+
+    1. Ensure the script is executable by invoking "chmod u+x remove.py".
+    2. Get the help texts by invoking "./remove.py --help".
+"""
+
+print("Hello World!")
